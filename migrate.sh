b629846dc16b807054c53136ee260059cbbfcfa5
Vadim Sikora <vadim.sikora@divio.ch>
2019-03-18 18:30:28 +0100

    or not

--- migrate.sh
+++ migrate.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 # This script only exists for compatibility reasons to a legacy deploy system and
 # will be removed. Please use the "start migrate" command to run migrations.
 # Custom migrations can be added through the MIGRATION_COMMANDS setting.
